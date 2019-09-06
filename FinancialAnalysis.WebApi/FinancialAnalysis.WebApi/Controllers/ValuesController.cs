a5573a85286560114218a26587ce67c0fd630717
Paulina Żurawska <zurawskapa@gmail.com>
2019-09-06 13:01:44 +0200

    Changed name of project to FinancialAnalysis.WebApi.

--- FinancialAnalysis.WebApi/FinancialAnalysis.WebApi/Controllers/ValuesController.cs
+++ FinancialAnalysis.WebApi/FinancialAnalysis.WebApi/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Project.Controllers
+namespace FinancialAnalysis.WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
